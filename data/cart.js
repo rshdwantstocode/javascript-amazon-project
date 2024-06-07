b244379872cd73d0d41d970f820475e263364479
Rashid <rushseed02@gmail.com>
2024-06-07 15:14:09 +0800

    save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,22 @@
-export let cart  = [
-  {
-    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 2,
-  },
-  {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1,
-  }
-];
+export let cart  = JSON.parse(localStorage.getItem('cart')) 
+
+if (!cart) {
+  cart = [
+    {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+    },
+    {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+    }
+  ];
+}
+
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addtoCart(productId){
     let matchItem;
@@ -31,6 +40,8 @@ export function addtoCart(productId){
           quantity
       });
     };
+
+    saveToStorage();
 
     //Added paragraph sign
     let addedStyle = document.querySelector(`.js-added-cart-${productId}`);
@@ -58,4 +69,6 @@ export function removefromCart(productId) {
   });
   
   cart = newCart;
+
+  saveToStorage();
 }
