b2ab891dc98505340a0511923284ea3f8615c8fc
Rashid <rushseed02@gmail.com>
2024-06-17 10:51:00 +0800

    Remove dafault test and add money test

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import { moneyMatters } from "../scripts/utils/money.js";
+
+describe('test suite: formatCurrency', ()=> {
+    it('convert cents into dollars', ()=>{
+        expect(moneyMatters(2095)).toEqual('20.95');
+    });
+
+    it('works with zero', ()=>{
+        expect(moneyMatters(0)).toEqual('0.00');
+    });
+
+    it('rounds up to the nearest cent', ()=>{
+        expect(moneyMatters(2000.5)).toEqual('20.01');
+    });
+});
